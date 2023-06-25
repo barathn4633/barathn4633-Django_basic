cd3d99d46c76d0be11e7026b1e8d0d2e14e694bb
barathn4633 <barathngowda@gmail.com>
2023-06-25 19:42:27 +0530

    like and dislike

--- myprj/settings.py
+++ myprj/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rhino.helix001@gmail.com'
-EMAIL_HOST_PASSWORD = 'szrcstvofeercrms'
+EMAIL_HOST_USER = 'yourmail.gmail.com'
+EMAIL_HOST_PASSWORD = 'password@yourmail.com'
